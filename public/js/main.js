4294b0981cad69cfc136f9ebc2507d92b5fb829a
ntun7729 <ntun77.19@gmail.com>
2026-05-17 07:04:16 +0630

    Force mobile class on Android browsers

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,16 @@
 import { setupChat } from "./chat.js";
 import { getChatElements } from "./dom.js";
 
+applyDeviceClass();
 setupChat(getChatElements());
+
+function applyDeviceClass() {
+  const isTouchDevice = window.matchMedia("(hover: none), (pointer: coarse)").matches;
+  const isNarrowScreen = window.innerWidth <= 1180;
+  const isAndroid = /Android/i.test(navigator.userAgent);
+
+  if (isTouchDevice || isNarrowScreen || isAndroid) {
+    document.documentElement.classList.add("force-mobile");
+    document.body.classList.add("force-mobile");
+  }
+}
