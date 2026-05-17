0a59e390fa7419179eee662c5d73b52ede0a36dd
ntun7729 <ntun77.19@gmail.com>
2026-05-17 09:08:44 +0630

    Add search environment bindings

--- src/types/env.ts
+++ src/types/env.ts
@@ -4,4 +4,7 @@ export interface Env {
   NVIDIA_API_KEY?: string;
   AI_BASE_URL: string;
   AI_MODEL: string;
+  SEARCH_API_KEY?: string;
+  BRAVE_SEARCH_API_KEY?: string;
+  SEARCH_PROXY_URL?: string;
 }
